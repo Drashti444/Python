1236c39c22f3527c96c6bc139dcaa8ef4fdf698b
Drashti <79237108+Drashti444@users.noreply.github.com>
2024-06-28 16:44:05 -0400

    Update Decorators.py

--- Functions/Decorators.py
+++ Functions/Decorators.py
@@ -1,1 +1,14 @@
+def Decorator_func(func):
+    def wrapper_func():
+        return func()
+    return wrapper_func
 
+@Decorator_func
+def display():
+    print("Good Morning!")
+
+f1 = Decorator_func(display)
+print(f1.__name__)
+f1()
+
+display()
