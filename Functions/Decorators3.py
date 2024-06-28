82960135cc9f44b5d34e150d8f91630564b2c161
Drashti <79237108+Drashti444@users.noreply.github.com>
2024-06-28 18:06:35 -0400

    Create Decorators3.py

---
+++ Functions/Decorators3.py
@@ -0,0 +1,43 @@
+import time
+
+
+def Time_taken(func):
+    import time
+    def wrapper(args):
+        # print(f"{args}")
+        start_time = time.time()
+        # print(f"Start time: {start_time}")
+        result = func(args)
+        # print(time.time())
+        end_time = time.time()-start_time
+        print(f"the factorial of {args} is {result}, and time taken for it is: {end_time}")
+    return wrapper
+
+
+
+#recursion funtion of factorial
+def factorial(num):
+    # print("Inside factorial")
+    # print(num)
+    if num == 1:
+        return num
+    else:
+        return num * factorial(num-1)
+
+#Factorial function using loop
+@Time_taken
+def Factorial(num):
+    time.sleep(1)
+    # print("Inside Factorial")
+    # print(num)
+    result = 1
+    for i in range(1,num+1):
+        result *= i
+    return result
+
+num = 5
+result = factorial(num)
+print("Result of Recursion function:",result)
+# Ans = Factorial(num)
+# print(Ans)
+Factorial(num)
